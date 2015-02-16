fc48890fe1be2a5d094849319ff6791adee9be17
Mark Wotton <mwotton@gmail.com>
2015-02-16 16:28:38 +0700

    cleanup, going back to file-embed

--- webcomplete.hs
+++ webcomplete.hs
@@ -5,17 +5,32 @@ module Main
 ) where
 
 import           Control.Applicative         ((<$>))
+import           Control.Monad.IO.Class      (liftIO)
 import qualified Data.ByteString.Char8       as BS
+import qualified Data.ByteString.Lazy        as BL
+import           Data.FileEmbed              (embedFile)
+import           Data.Text.Lazy.Encoding     (decodeUtf8)
 import           Network.Wai.Middleware.Cors
-import           Paths_webcomplete
 import           Text.FastEdit               (buildDict)
 import           Web.Scotty
 
+
+dict = $(embedFile "/usr/share/dict/british-english")
+
+neighbours = buildDict 3 . map BS.unpack  $ BS.lines dict
+
+index =  $(embedFile "index.html")
+
 main :: IO ()
 main = do
-  neighbours <-  buildDict 2 . map BS.unpack  . BS.lines
-                 <$> (BS.readFile =<< getDataFileName "/usr/share/dict/words")
+  let indexText = decodeUtf8 (BL.fromChunks [index])
+  
+  print (neighbours "test")
   scotty 8080 $ do
     middleware simpleCors
     get  "/complete" $ do
-      json . neighbours =<< param "partial"
+      -- log queries to stdout
+      params >>= liftIO . print
+      json . neighbours =<< param "term"
+
+    get "/" $ html indexText
